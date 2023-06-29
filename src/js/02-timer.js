47a3b35478b1c881d1c4792c1dc5fb52cf091fcf
SvAmi1 <svmoshko877@gmail.com>
2023-06-29 17:40:19 +0300

    in work

---
+++ src/js/02-timer.js
@@ -0,0 +1,58 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const calendar = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+startBtn.disabled = true;
+
+const currentDate = Date.now();
+const deltaDate = selectedDate - currentDate;
+let selectedDate = null;
+let intervalId = null;
+
+Notify.failure('Please choose a date in the future');
+
+const timer = {
+    start() {
+        if (selectedDate)
+    }
+}
+flatpickr(calendar, {enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },});
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+ function addLeadingZero(value) {
+return String(value).padStart(2,"0");
+  }
+//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
