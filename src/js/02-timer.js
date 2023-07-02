69700e1437adbc75a0e988075df024b0ab22a25b
SvAmi1 <svmoshko877@gmail.com>
2023-07-02 18:07:20 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,39 +4,62 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const calendar = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
-startBtn.disabled = true;
 
-const currentDate = Date.now();
-const deltaDate = selectedDate - currentDate;
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let currentDate = new Date();
 let selectedDate = null;
+let deltaDate = null;
 let intervalId = null;
 
-Notify.failure('Please choose a date in the future');
-
-const timer = {
-    start() {
-        if (selectedDate)
-    }
-}
-flatpickr(calendar, {enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },});
+startBtn.disabled = true;
+startBtn.addEventListener('click', startTimer);
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    deltaDate = selectedDate - currentDate;
 
-  function convertMs(ms) {
+    if (selectedDate <= currentDate) {
+      Notify.failure('Please choose a date in the future');
+      return;
+    }
+    else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(calendar, options);
+
+function startTimer() {
+  intervalId = setInterval(() => {
+  
+  const time = convertMs(deltaDate);
+  updateTimer(time);
+  }, 1000);
+  startBtn.disabled = true;
+  stopTimer()
+}
+
+function stopTimer() {
+  if (daysEl.textContent === '00' &&
+    hoursEl.textContent === '00' &&
+    minutesEl.textContent === '00' &&
+    secondsEl.textContent === '00') {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+}
+
+function convertMs(ms) {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
@@ -53,6 +76,10 @@ const options = {
  function addLeadingZero(value) {
 return String(value).padStart(2,"0");
   }
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+  function updateTimer({ days, hours, minutes, seconds }) {
+    daysEl.textContent = days.toString();
+    hoursEl.textContent = hours.toString();
+    minutesEl.textContent = minutes.toString();
+    secondsEl.textContent = seconds.toString();
+  }
