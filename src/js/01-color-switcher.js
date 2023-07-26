394e7fa0d9d20c4c7c621e99f7f1e407a6267e54
SvAmi1 <svmoshko877@gmail.com>
2023-07-27 02:59:54 +0300

    9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,30 +1,120 @@
-const startBtn = document.querySelector('[data-start]');
-const stopBtn = document.querySelector('[data-stop]');
-const bodyEl = document.querySelector('body');
+// const startBtn = document.querySelector('[data-start]');
+// const stopBtn = document.querySelector('[data-stop]');
+// const bodyEl = document.querySelector('body');
 
-let intervalId = null;
+// let intervalId = null;
 
-startBtn.addEventListener('click', changeColor);
-stopBtn.addEventListener('click', cancelChangeColor);
+// startBtn.addEventListener('click', changeColor);
+// stopBtn.addEventListener('click', cancelChangeColor);
 
-stopBtn.disabled = true;
+// stopBtn.disabled = true;
 
-  function changeColor() {
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
+//   function changeColor() {
+//     startBtn.disabled = true;
+//     stopBtn.disabled = false;
 
-    intervalId = setInterval(() => {
-      bodyEl.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+//     intervalId = setInterval(() => {
+//       bodyEl.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//   }
+
+//   function cancelChangeColor() {
+//     stopBtn.disabled = true;
+//     startBtn.disabled = false;
+
+//     clearInterval(intervalId);
+//   }
+
+//   function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+//   }
+
+
+import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper';
+
+const mkBox = document.querySelector('.swiper-wrapper');
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/events';
+
+async function fetchMk() {
+  let resp = await fetch(`${BASE_URL}`);
+  if (!resp.ok) {
+    throw new Error(resp.statusText);
   }
+  return await resp.json();
+}
 
-  function cancelChangeColor() {
-    stopBtn.disabled = true;
-    startBtn.disabled = false;
-
-    clearInterval(intervalId);
+async function renderMk() {
+  try {
+    const arrMk = await fetchMk();
+    const markup = arrMk
+      .map(evt => {
+        return `
+         <div class="mk-card swiper-slide">
+           <ul class="mk-list">
+           <li class="mk-item chief">
+           <div class="mk-photo-wrapper chief">
+            <img class="chief-img img" src="${evt.cook.imgUrl}" alt="${evt.cook.name}" width="80" /></div></li>
+            <li class="mk-item dish">
+            <div class="mk-photo-wrapper preview">
+            <img class="preview-img img" src="${evt.topic.previewUrl}" alt="dish" width="200"/>
+              <div class="mk-info">
+                <h4 class="mk-name">${evt.topic.name}</h4>
+                <p class="mk-region">${evt.topic.area}</p>
+              </div></div>
+            </li>
+            <li class="mk-item">
+            <div class="mk-photo-wrapper zoom">
+            <img class="dish-img img" src="${evt.topic.imgUrl}" alt="dish" width="200"/></div></li>
+          </ul> 
+          </div>
+        `;
+      })
+      .join("");
+    mkBox.innerHTML = markup;
+  } catch (error) {
+    console.error("Error:", error);
   }
+}
 
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+function addMkInfo() {
+  renderMk();
+  const swiper = new Swiper(".swiper", {
+    // slidesPerView: 1,
+    // slidesPerGroup: 1,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    direction: 'horizontal',
+    // rewind: true,
+    loop: true,
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    //   draggable: true,
+    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: 0.65,
+ 
+    speed: 600,
+
+    breakpoints: {
+      375: {
+      slidesPerView: 0.65,
+        spaceBetween: 8
+      },
+      768: {
+        slidesPerView: 0.85,
+        spaceBetween: 16
+      },
+      1280: {
+        slidesPerView: 0.81,
+        spaceBetween: 16
+      }
+    }
+  });
+  console.log(swiper);
+}
+addMkInfo(); 
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,91 +2,219 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const calendar = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('button[data-start]');
+// const calendar = document.querySelector('#datetime-picker');
+// const startBtn = document.querySelector('button[data-start]');
 
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
+// const daysEl = document.querySelector('[data-days]');
+// const hoursEl = document.querySelector('[data-hours]');
+// const minutesEl = document.querySelector('[data-minutes]');
+// const secondsEl = document.querySelector('[data-seconds]');
 
-let currentDate = null;
-let selectedDate = null;
-let deltaDate = null;
-let intervalId = null;
+// let currentDate = null;
+// let selectedDate = null;
+// let deltaDate = null;
+// let intervalId = null;
 
-startBtn.disabled = true;
-startBtn.addEventListener('click', startTimer);
+// startBtn.disabled = true;
+// startBtn.addEventListener('click', startTimer);
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    selectedDate = selectedDates[0];
-    currentDate = Date.now();
-    deltaDate = selectedDate - currentDate;
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     selectedDate = selectedDates[0];
+//     currentDate = Date.now();
+//     deltaDate = selectedDate - currentDate;
 
-    if (selectedDate <= currentDate) {
-      Notify.failure('Please choose a date in the future');
-      return;
-    }
-    else {
-      startBtn.disabled = false;
-    }
-  },
+//     if (selectedDate <= currentDate) {
+//       Notify.failure('Please choose a date in the future');
+//       return;
+//     }
+//     else {
+//       startBtn.disabled = false;
+//     }
+//   },
+// };
+
+// const datePicker = flatpickr(calendar, options);
+
+// function startTimer() {
+//   intervalId = setInterval(() => {
+//   selectedDate = datePicker.selectedDates[0].getTime();
+//   currentDate = Date.now();
+//   deltaDate = selectedDate - currentDate;
+//   const time = convertMs(deltaDate);
+//   updateTimer(time);
+//   calendar.disabled = true;
+//   if (deltaDate < 1000) {
+//     stopTimer();
+//   }
+//   }, 1000);
+//   startBtn.disabled = true;
+//   // calendar.disabled = false;
+//   }
+
+// function stopTimer() {
+//   if (daysEl.textContent === '00' &&
+//     hoursEl.textContent === '00' &&
+//     minutesEl.textContent === '00' &&
+//     secondsEl.textContent === '00') {
+//   clearInterval(intervalId);
+//   startBtn.disabled = false;
+// }
+// }
+
+// function convertMs(ms) {
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
+  
+//     const days = addLeadingZero(Math.floor(ms / day));
+//     const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//     const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//     const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+//     return { days, hours, minutes, seconds };
+//   }
+  
+//  function addLeadingZero(value) {
+// return String(value).padStart(2,"0");
+//   }
+
+//   function updateTimer({ days, hours, minutes, seconds }) {
+//     daysEl.textContent = days.toString();
+//     hoursEl.textContent = hours.toString();
+//     minutesEl.textContent = minutes.toString();
+//     secondsEl.textContent = seconds.toString();
+//   }
+
+
+// const categroyList = document.querySelector('.category-list');
+const refs = {
+  categoryList: document.querySelector('.category-list'),
+  categoryContainer: document.querySelector('.category-container'),
+  allCategoryButton: document.querySelector('.all-category-button'),
 };
 
-const datePicker = flatpickr(calendar, options);
+import axios from 'axios';
 
-function startTimer() {
-  intervalId = setInterval(() => {
-  selectedDate = datePicker.selectedDates[0].getTime();
-  currentDate = Date.now();
-  deltaDate = selectedDate - currentDate;
-  const time = convertMs(deltaDate);
-  updateTimer(time);
-  calendar.disabled = true;
-  if (deltaDate < 1000) {
-    stopTimer();
-  }
-  }, 1000);
-  startBtn.disabled = true;
-  // calendar.disabled = false;
-  }
+const CATEGORIE_URL ='https://tasty-treats-backend.p.goit.global/api/categories';
 
-function stopTimer() {
-  if (daysEl.textContent === '00' &&
-    hoursEl.textContent === '00' &&
-    minutesEl.textContent === '00' &&
-    secondsEl.textContent === '00') {
-  clearInterval(intervalId);
-  startBtn.disabled = false;
+export async function fetchCategories() {
+  try{
+    const categories = await axios.get(CATEGORIE_URL);
+    
+    return categories.data;
+
+  }catch(error){
+    console.log(error);
+  };
+};
+
+
+// function fillCategoriesList() {
+//      fetchCategories()
+//     .then(({_id, name}) =>
+//     renderCategoriesList({_id, name}))
+//     .catch(error => {console.log(error);
+//     Notify.failure('Something went wrong. Please try again');})
+// };
+
+
+// fillCategoriesList();
+
+
+async function renderCategoriesList() {
+  try { 
+    const arrMk = await  fetchCategories();
+    // console.log(arrMk);
+    const markup = arrMk
+    .map(evt => {
+      return `<li class='cat-items'>
+      <button class='category-btn' value = ${evt._id}>${evt.name}</button>
+    </li>`}).join('');
+  refs.categoryList.innerHTML = markup;
+} catch (error) {
+  console.error("Error:", error);
 }
+
 }
 
-function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = addLeadingZero(Math.floor(ms / day));
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-  
-    return { days, hours, minutes, seconds };
-  }
-  
- function addLeadingZero(value) {
-return String(value).padStart(2,"0");
+renderCategoriesList();
+//////
+
+///////
+refs.categoryContainer.addEventListener('click', onBtnCLick);
+
+let lastClickedBtn = null;
+
+
+function onBtnCLick(event) {
+  const Btn = event.target;
+
+  if (Btn.nodeName !== 'BUTTON') {
+    return;
   }
 
-  function updateTimer({ days, hours, minutes, seconds }) {
-    daysEl.textContent = days.toString();
-    hoursEl.textContent = hours.toString();
-    minutesEl.textContent = minutes.toString();
-    secondsEl.textContent = seconds.toString();
+  if (lastClickedBtn) {
+    lastClickedBtn.classList.remove('active');
   }
+
+  if (Btn === refs.allCategoryButton) {
+    removeActiveClassFromAllButtons();
+  } else {
+    refs.allCategoryButton.classList.remove('active');
+  }
+
+  Btn.classList.add('active');
+  lastClickedBtn = Btn;
+};
+
+
+function removeActiveClassFromAllButtons() {
+  const buttons = refs.categoryList.querySelectorAll('button');
+
+  buttons.forEach(button => {
+    button.classList.remove('active');
+  });
+};
+
+
+refs.categoryList.addEventListener('click', event => {
+  if (!event.target.classList.contains('category-btn')) {
+    event.stopPropagation();
+  }
+});
+
+const recipesList = document.querySelector('.recipes-list');
+
+refs.categoryList.addEventListener('click', handleCategoryCards);
+
+function handleCategoryCards() {
+  event => {
+    if (!event.target.value.contains('category-btn')) {
+      event.stopPropagation();
+    }
+  }
+}
+
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes?{id}';
+const ID_CARDS_URL ='/recipes/{id}';
+
+export async function fetchIdCards() {
+  try{
+    const idCard = await axios.get(BASE_URL);
+    console.log(idCard);
+    return idCard.data;
+    
+
+  }catch(error){
+    console.log(error);
+  };
+};
+
+fetchIdCards().then(data => data.map((data) => {
+console.log(data.results.category)}))
