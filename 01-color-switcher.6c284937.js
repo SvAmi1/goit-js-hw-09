8ca49582fa54dff8405d4b04554cc5136b14aea7
SvAmi1 <124162814+SvAmi1@users.noreply.github.com>
2023-07-27 00:00:24 +0000

    Deploying to gh-pages from @ SvAmi1/goit-js-hw-09@394e7fa0d9d20c4c7c621e99f7f1e407a6267e54 🚀

--- 01-color-switcher.6c284937.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(a)})),t.disabled=!0;
-//# sourceMappingURL=01-color-switcher.6c284937.js.map
